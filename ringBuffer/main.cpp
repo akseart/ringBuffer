4b8c871895a8517434e96f042b30b6ce0c051f4f
akseart <akseart@ya.ru>
2018-03-04 16:30:14 +0300

    Initial Commit

---
+++ ringBuffer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ringBuffer
+//
+//  Created by Артем on 04.03.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
